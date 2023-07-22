4e66a79d21282fbe23fb5564fa258398993ba1b5
Lucas Lucovas <lucaslucovas@gmail.com>
2023-07-22 19:50:34 -0300

    finalizando proyecto final

--- src/components/firebase/config.js
+++ src/components/firebase/config.js
@@ -5,15 +5,17 @@ import { getFirestore } from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDudKBADDK87JzIadUToLRoP2bWUS9C1CU",
-  authDomain: "reactecommerce-c8f84.firebaseapp.com",
-  projectId: "reactecommerce-c8f84",
-  storageBucket: "reactecommerce-c8f84.appspot.com",
-  messagingSenderId: "448651589142",
-  appId: "1:448651589142:web:98f91f6347719404faa737",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
+
+export default firebaseConfig;
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app)
